317e503057b5662bd797b88a8f1df8a64da226ab
Олег Ільченко <olegilcenko@MacBook-Air-Oleg.local>
2022-10-11 19:42:50 +0300

    Backend.Service and controller for user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,84 @@
-import { Injectable } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException
+} from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { genSalt, hash } from 'bcryptjs'
+import { Repository } from 'typeorm'
+import { SubscriptionEntity } from './subscription.entity'
+import { UserDto } from './user.dto'
+import { UserEntity } from './user.entity'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	constructor(
+		@InjectRepository(UserEntity)
+		private readonly userRepository: Repository<UserEntity>,
+		@InjectRepository(SubscriptionEntity)
+		private readonly subscriptionRepository: Repository<SubscriptionEntity>
+	) {}
+
+	async byId(id: number) {
+		const user = await this.userRepository.findOne({
+			where: {
+				id
+			},
+			relations: {
+				videos: true,
+				subscriptions: {
+					toChannel: true
+				}
+			},
+			order: {
+				createdAt: 'DESC'
+			}
+		})
+
+		if (!user) throw new NotFoundException('User is not found!')
+		return user
+	}
+
+	async updateProfile(id: number, dto: UserDto) {
+		const user = await this.byId(id)
+
+		const IsSameUser = await this.userRepository.findOneBy({ email: dto.email })
+		if (IsSameUser && id !== IsSameUser.id)
+			throw new BadRequestException('Email registered')
+
+		if (dto.password) {
+			const salt = await genSalt(10)
+			user.password = await hash(dto.password, salt)
+		}
+
+		user.email = dto.email
+		user.name = dto.name
+		user.description = dto.description
+		user.avatarPath = dto.avatarPath
+
+		return this.userRepository.save(user)
+	}
+
+	async subscribe(id: number, channelId: number) {
+		const data = {
+			toChannel: { id: channelId },
+			fromUser: { id }
+		}
+
+		const isSubscribed = await this.subscriptionRepository.findOneBy(data)
+
+		if (!isSubscribed) {
+			const newSubscription = await this.subscriptionRepository.create(data)
+			await this.subscriptionRepository.save(newSubscription)
+
+			return true
+		}
+
+		await this.subscriptionRepository.delete(data)
+		return false
+	}
+
+	async getAll() {
+		return this.userRepository.find()
+	}
+}
